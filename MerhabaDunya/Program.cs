f87e35949b063da72b084636998cefb981ed4bee
KodluYorum <info@kodluyorum.dev>
2021-05-09 15:25:47 +0300

    Add project files.

---
+++ MerhabaDunya/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MerhabaDunya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya!");
+        }
+    }
+}
